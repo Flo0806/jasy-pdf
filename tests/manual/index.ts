cfa8b84369e60c1540a117cd08d3c4c0c60c8e10
Florian Heuberger <fh@flogersoft.de>
2024-09-09 22:49:15 +0200

    added font family to tests

--- tests/manual/index.ts
+++ tests/manual/index.ts
@@ -2,6 +2,7 @@ import { PDFDocumentElement } from "../../src/lib/elements/pdf-document-element"
 import { PageElement } from "../../src/lib/elements/page-element";
 import { TextElement } from "../../src/lib/elements/text-element";
 import { PDFRenderer } from "../../src/lib/renderer/pdf-renderer";
+import fs from "fs";
 
 const pdf = new PDFDocumentElement([
   new PageElement([
@@ -17,10 +18,19 @@ const pdf = new PDFDocumentElement([
       y: 250,
       fontSize: 18,
       color: [0, 0, 255],
-      content: "This is a test. YES!",
+      fontFamily: "Courier",
+      content:
+        "This is a test. YES! It's a very long text. I wanna see if the pdf will be break into a new line!",
     }),
   ]),
 ]);
 
 const renderedPDF = PDFRenderer.render(pdf);
 console.log(renderedPDF);
+fs.writeFile("C:/Users/fh/Downloads/test.pdf", renderedPDF, (err) => {
+  if (err) {
+    console.error("Error writing file:", err);
+  } else {
+    console.log("PDF saved successfully!");
+  }
+});
