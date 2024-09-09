4d5f2febb60c313c8cba54bb2ab28afae4c33619
Florian Heuberger <fh@flogersoft.de>
2024-09-09 20:11:48 +0200

    added some text elements to creating a pdf

--- tests/manual/index.ts
+++ tests/manual/index.ts
@@ -1,23 +1,23 @@
-import { PDFDocument } from "../../src/lib/elements/pdf-document-element";
-import { Page } from "../../src/lib/elements/page-element";
-import { Text } from "../../src/lib/elements/text-element";
+import { PDFDocumentElement } from "../../src/lib/elements/pdf-document-element";
+import { PageElement } from "../../src/lib/elements/page-element";
+import { TextElement } from "../../src/lib/elements/text-element";
 import { PDFRenderer } from "../../src/lib/renderer/pdf-renderer";
 
-const pdf = new PDFDocument([
-  new Page([
-    new Text({
+const pdf = new PDFDocumentElement([
+  new PageElement([
+    new TextElement({
       x: 100,
       y: 200,
       fontSize: 24,
       color: [255, 0, 0],
       content: "Hello, JasyPDF!",
     }),
-    new Text({
+    new TextElement({
       x: 150,
       y: 250,
       fontSize: 18,
       color: [0, 0, 255],
-      content: "This is a test.",
+      content: "This is a test. YES!",
     }),
   ]),
 ]);
