98b962ad138e76c87aba5aa69171efe8a7aa7b03
Florian Heuberger <fh@flogersoft.de>
2024-09-09 22:48:26 +0200

    removed unnecessary render functions

--- src/lib/elements/page-element.ts
+++ src/lib/elements/page-element.ts
@@ -2,8 +2,4 @@ import { TextElement } from "./text-element";
 
 export class PageElement {
   constructor(public elements: TextElement[]) {}
-
-  render() {
-    return this.elements.map((element) => element.render()).join("\n");
-  }
 }
--- src/lib/elements/text-element.ts
+++ src/lib/elements/text-element.ts
@@ -1,7 +1,10 @@
+import { TextRenderer } from "../renderer/text-renderer";
+
 type TextElementParams = {
   x: number;
   y: number;
   fontSize: number;
+  fontFamily?: string;
   content: string;
   color?: [number, number, number]; // optional param
 };
@@ -10,21 +13,30 @@ export class TextElement {
   private x: number;
   private y: number;
   private fontSize: number;
+  private fontFamily: string;
   private color: [number, number, number];
   private content: string;
+  private width: number;
+  private height: number;
 
   constructor({
     x,
     y,
     fontSize,
     content,
+    fontFamily = "Helvetica",
     color = [0, 0, 0],
   }: TextElementParams) {
     this.x = x;
     this.y = y;
     this.fontSize = fontSize;
+    this.fontFamily = fontFamily;
     this.color = color;
     this.content = content;
+
+    const { width, height } = TextRenderer.getTextSize(this);
+    this.width = width;
+    this.height = height;
   }
 
   getProps() {
@@ -32,14 +44,13 @@ export class TextElement {
       x: this.x,
       y: this.y,
       fontSize: this.fontSize,
+      fontFamily: this.fontFamily,
       color: this.color,
       content: this.content,
     };
   }
 
-  render() {
-    return `BT /F1 ${this.fontSize} Tf ${this.color.join(" ")} rg ${this.x} ${
-      this.y
-    } Td (${this.content}) Tj ET`;
+  getSize() {
+    return { x: this.x, y: this.y, width: this.width, height: this.height };
   }
 }
