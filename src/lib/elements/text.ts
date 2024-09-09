fbdfda2027aa10feb77287f7fc0809f0d7b20597
Florian Heuberger <fh@flogersoft.de>
2024-09-09 18:42:05 +0200

    changed text elements params and constructor

--- src/lib/elements/text.ts
+++ src/lib/elements/text.ts
@@ -1,11 +1,41 @@
+type TextElementParams = {
+  x: number;
+  y: number;
+  fontSize: number;
+  content: string;
+  color?: [number, number, number]; // Optionaler Parameter mit Standardwert
+};
+
 export class Text {
-  constructor(
-    public x: number,
-    public y: number,
-    public fontSize: number,
-    public color: [number, number, number],
-    public content: string
-  ) {}
+  private x: number;
+  private y: number;
+  private fontSize: number;
+  private color: [number, number, number];
+  private content: string;
+
+  constructor({
+    x,
+    y,
+    fontSize,
+    content,
+    color = [0, 0, 0],
+  }: TextElementParams) {
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.color = color;
+    this.content = content;
+  }
+
+  getProps() {
+    return {
+      x: this.x,
+      y: this.y,
+      fontSize: this.fontSize,
+      color: this.color,
+      content: this.content,
+    };
+  }
 
   render() {
     return `BT /F1 ${this.fontSize} Tf ${this.color.join(" ")} rg ${this.x} ${
