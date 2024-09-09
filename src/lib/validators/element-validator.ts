3a21fc5797db75ddb6d8e073a7fd1d669c0038db
Florian Heuberger <fh@flogersoft.de>
2024-09-09 20:11:33 +0200

    changed a little in the first validation function

--- src/lib/validators/element-validator.ts
+++ src/lib/validators/element-validator.ts
@@ -1,11 +1,11 @@
-import { PDFDocument } from "../elements/pdf-document-element";
+import { PDFDocumentElement } from "../elements/pdf-document-element";
 
 export class Validator {
-  static validateDocument(document: PDFDocument) {
-    // Hier können Validierungen hinzugefügt werden, z.B. keine PDFDocument-Kinder in Text-Elementen
+  static validateDocument(document: PDFDocumentElement) {
+    // Later more validation....
     document.pages.forEach((page) => {
       page.elements.forEach((element) => {
-        if (element instanceof PDFDocument) {
+        if (element instanceof PDFDocumentElement) {
           throw new Error(
             "PDFDocument cannot be nested inside another element."
           );
