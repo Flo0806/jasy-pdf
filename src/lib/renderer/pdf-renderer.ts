1a99a8a6d008d213b4cc21386e4905d19483b11c
Florian Heuberger <fh@flogersoft.de>
2024-09-17 20:10:09 +0200

    added new renderer to registry

--- src/lib/renderer/pdf-renderer.ts
+++ src/lib/renderer/pdf-renderer.ts
@@ -2,13 +2,14 @@ import { PDFDocumentElement } from "../elements/pdf-document-element";
 import { PDFDocumentRenderer } from "./pdf-document-renderer";
 import { PDFObjectManager } from "../utils/pdf-object-manager";
 import { RendererRegistry } from "../utils/renderer-registry";
-import { ExpandedElement, TextElement } from "../elements";
+import { ExpandedElement, PaddingElement, TextElement } from "../elements";
 import { TextRenderer } from "./text-renderer";
 import { ContainerElement } from "../elements/container-element";
 import { RectangleElement } from "../elements/rectangle-element";
 import { ContainerRenderer } from "./container-renderer";
 import { RectangleRenderer } from "./rectangle-renderer";
 import { ExpandedRenderer } from "./expanded-renderer";
+import { PaddingRenderer } from "./padding-renderer";
 
 export class PDFRenderer {
   static render(document: PDFDocumentElement): string {
@@ -19,6 +20,7 @@ export class PDFRenderer {
     RendererRegistry.register(ContainerElement, ContainerRenderer.render);
     RendererRegistry.register(RectangleElement, RectangleRenderer.render);
     RendererRegistry.register(ExpandedElement, ExpandedRenderer.render);
+    RendererRegistry.register(PaddingElement, PaddingRenderer.render);
 
     let pdfContent = "";
 
