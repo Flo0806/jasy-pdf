f946ce9d2c477987d55c69896787acb940d4eb21
Florian Heuberger <fh@flogersoft.de>
2024-09-22 12:36:40 +0200

    added some logic to creating only 'one' pdf manager globally

--- src/lib/utils/pdf-object-manager-decorator.ts
+++ src/lib/utils/pdf-object-manager-decorator.ts
@@ -3,42 +3,26 @@ import { PDFObjectManager } from "./pdf-object-manager";
 
 const OBJECT_MANAGER_KEY = Symbol("PDFObjectManager");
 
-// Dekorator zur Injektion des ObjectManagers
+// Decorator to inject the `PDFObjectManager` globaly
+// This helps me a lot: https://blog.jannikwempe.com/typescript-class-decorators-incl-dependency-injection-example
 export function InjectObjectManager() {
   return function (target: any, propertyKey: string) {
     // In den Metadaten nach dem ObjectManager suchen
     Object.defineProperty(target, propertyKey, {
       get() {
-        let manager = Reflect.getMetadata(OBJECT_MANAGER_KEY, target);
+        let manager = Reflect.getMetadata(OBJECT_MANAGER_KEY, Reflect);
         if (!manager) {
           // Falls kein Manager existiert, erstellen und setzen
           manager = new PDFObjectManager();
-          Reflect.defineMetadata(OBJECT_MANAGER_KEY, manager, target);
+          Reflect.defineMetadata(OBJECT_MANAGER_KEY, manager, Reflect);
         }
         return manager;
       },
       set(value: PDFObjectManager) {
-        Reflect.defineMetadata(OBJECT_MANAGER_KEY, value, target);
+        Reflect.defineMetadata(OBJECT_MANAGER_KEY, value, Reflect);
       },
       enumerable: true,
       configurable: true,
     });
   };
 }
-
-// import { PDFObjectManager } from "./pdf-object-manager";
-
-// // This helps me a lot: https://blog.jannikwempe.com/typescript-class-decorators-incl-dependency-injection-example
-// export function InjectObjectManager() {
-//   return function InjectObjectManager<T extends { new (...args: any[]): {} }>(
-//     constructor: T
-//   ): T | void {
-//     // Replacing the original constructor with a new one that provides the injections from the Container
-//     return class extends constructor {
-//       constructor(...args: any[]) {
-//         super(...args);
-//         (this as any)._objectManager = new PDFObjectManager();
-//       }
-//     };
-//   };
-// }
