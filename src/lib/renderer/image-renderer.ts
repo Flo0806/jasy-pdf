d3bb5d1ddb62e7828c1f6e9138838cde2fdc144c
Florian Heuberger <fh@flogersoft.de>
2024-09-23 16:21:37 +0200

    added first part of image renderer

--- src/lib/renderer/image-renderer.ts
+++ src/lib/renderer/image-renderer.ts
@@ -1,50 +1,40 @@
-import { CustomImage, ImageElement } from "../elements/image-element";
+import { ImageElement } from "../elements/image-element";
 import { PDFObjectManager } from "../utils/pdf-object-manager";
-import { RendererRegistry } from "../utils/renderer-registry";
 
 export class ImageRenderer {
   static async render(
     imageElement: ImageElement,
     objectManager: PDFObjectManager
-  ): Promise<number> {
-    // Zuerst das Bild laden und konvertieren
+  ): Promise<string> {
+    // Load the image and convert it in a binary string
     const { x, y, width, height, image } = imageElement.getProps();
-    await image.loadAndConvertImage(); // Image in Base64 laden
-    const imageType = await image.getImageType(); // Beispielmethode für JPEG/PNG
-    const base64Data = await image.getBase64Data();
+    await image.loadAndConvertImage(); // Load and initialize the image
+    const imageType = await image.getImageType(); // For the moment we can handle `png` and `jpg/jpeg` files
+    const fileData = await image.getFileData();
+    const dimensions = await image.getImageDimensions();
 
-    // Hier wird das Bild als Objekt hinzugefügt, inklusive der Länge des Bildstreams
-    const imageObject = `
-      << /Type /XObject
-         /Subtype /Image
-         /Width ${width}
-         /Height ${height}
-         /ColorSpace /DeviceRGB
-         /BitsPerComponent 8
-         /Filter /${imageType}
-         /Length ${base64Data ? base64Data.length : 0} >>
-      stream
-      ${base64Data}
-      endstream
-    `;
+    if (!fileData) {
+      throw new Error("File data cannot be `null`");
+    }
 
-    // Füge das Bild als Objekt dem PDF hinzu und bekomme die Objektnummer zurück
-    const imageObjectNumber = objectManager.addObject(imageObject);
-
-    // Jetzt können wir das Bild rendern, indem wir es an die entsprechende Position im PDF setzen
-    const imagePlacement = `
-      q
-      ${width} 0 0 ${height} ${x} ${y} cm
-      /Im${imageObjectNumber} Do
-      Q
-    `;
-
-    // Füge die Bildplatzierung als Inhalt hinzu
-    const contentObjectNumber = objectManager.addObject(
-      `<</Length ${imagePlacement.length}>>\nstream\n${imagePlacement}endstream`
+    const imageObjectNumber = objectManager.registerImage(
+      dimensions.width,
+      dimensions.height,
+      imageType,
+      fileData
     );
 
-    // Gib die Objektnummer zurück, falls nötig
-    return contentObjectNumber;
+    // Now we can render the image... create the image placement code
+
+    const imagePlacement = `q
+${x} ${y + (height || 0) - 100} ${width} 100 re 
+W n 
+q
+${width} 0 0 ${height} ${x} ${y} cm
+/IM${imageObjectNumber} Do
+Q
+Q
+`;
+    return imagePlacement;
   }
 }
